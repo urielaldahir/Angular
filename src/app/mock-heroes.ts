c4d474ef761cce00695462e490ff1d1175ef4d3d
urielaldahir <112899169+urielaldahir@users.noreply.github.com>
2022-11-10 16:32:47 -0600

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from 'C:/Users/ASOFTWARE/Desktop/AngularProy/Angular/Heroes/src/app/heroes/hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', puntos: 1},
+  { id: 13, name: 'Bombasto', puntos: 1 },
+  { id: 14, name: 'Celeritas', puntos: 1 },
+  { id: 15, name: 'Magneta' , puntos: 1},
+  { id: 16, name: 'RubberMan', puntos: 1 },
+  { id: 17, name: 'Dynama' , puntos: 1},
+  { id: 18, name: 'Dr. IQ', puntos: 1 },
+  { id: 19, name: 'Magma' , puntos: 1},
+  { id: 20, name: 'Tornado' , puntos: 1}
+];
